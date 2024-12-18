4ea090e72b98b94066fe02b0847a71aa21d552b0
vineethdesetty <vineethchintu9@gmail.com>
2024-12-18 16:43:03 +0530

    Add project files.

---
+++ EmployeeData/Context/EmployeeDbContext.cs
@@ -0,0 +1,16 @@
+﻿using EmployeeData.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeData.Context
+{
+    public class EmployeeDbContext:DbContext
+    {
+        public EmployeeDbContext()
+        { }
+        public EmployeeDbContext(DbContextOptions<EmployeeDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Employee> Employees { get; set; }
+
+    }
+}
